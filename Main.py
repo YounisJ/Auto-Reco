4a00522112a89a8f37d4f18261bb0c9697d04409
YounisJ <muhammadyounis.bscskdkf22@iba-suk.edu.pk>
2025-02-23 21:15:56 +0500

    File Updated

--- Main.py
+++ Main.py
@@ -13,6 +13,7 @@ print(colored(banner, "cyan"))
 print(colored("\n\nThis tool is built for pentesters and bug hunters You \njust have to provide the URL of the website you want to\nperform reconnesance on", "blue"))
 
 
+
 # Get user input
 url = input(colored("\nEnter Website URL Please: ", "yellow")).strip()
 
